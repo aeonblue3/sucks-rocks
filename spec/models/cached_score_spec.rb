00bdc209ff17be8664bf3b4e2096b525d1ec2368
Chris Neitzer <chris@neitzer.com>
2012-07-04 00:38:19 -0400

    Added CachedScore model and database table

---
+++ spec/models/cached_score_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe CachedScore do
+  it "Remembers scores" do
+    CachedScore.save_score("microsoft", 7.5)
+    CachedScore.for_term("microsoft").should == 7.5
+  end
+  it "raises an exception if the term is not cached" do
+    expect do
+      CachedScore.for_term("microsoft")
+    end.to raise_error(CachedScore::NoScore)
+  end
+end
