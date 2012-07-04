bd3f2847f893c25f9a3d50b0e552a11f4ec642be
Chris Neitzer <chris@neitzer.com>
2012-07-03 23:52:53 -0400

    Added ScoreCache class

---
+++ spec/services/score_cache_spec.rb
@@ -0,0 +1,31 @@
+require_relative "../../app/services/score_cache"
+require_relative "../../app/services/rock_score"
+
+class CachedScore
+	class NoScore < RuntimeError; end
+end
+
+describe ScoreCache do
+	it "returns cached score if they exist" do
+		CachedScore.stub(:for_term).with("microsoft") { 5.5 }
+		ScoreCache.for_term("microsoft").should == 5.5
+	end
+
+	context "when the term is not cached" do
+		before do
+			RockScore.stub(:for_term).with("microsoft") { 5.5 }
+			CachedScore.stub(:for_term).with("microsoft").
+				and_raise(CachedScore::NoScore)
+			CachedScore.stub(:save_score)
+		end
+
+		it "recomputes score" do
+			ScoreCache.for_term("microsoft").should == 5.5
+		end
+
+		it "stores new score in the database" do
+			CachedScore.should_receive(:save_score).with("microsoft", 5.5)
+			ScoreCache.for_term("microsoft")
+		end
+	end
+end
