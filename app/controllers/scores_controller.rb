a9e7d20d2a607675f1c8fc465c92e3f5fefc8cf7
Chris Neitzer <chrisn@soholaunch.com>
2012-07-05 15:05:28 -0400

    Working on spec and controller for ScoresController

---
+++ app/controllers/scores_controller.rb
@@ -0,0 +1,7 @@
+class ScoresController < ApplicationController
+  def show
+    term = params[:term]
+    score = ScoreCache.for_term(term)
+    render :json => {:term => term, :score => score}
+  end
+end
---
+++ spec/controllers/scored_controller_spec.rb
@@ -0,0 +1,9 @@
+require "spec_helper"
+
+describe ScoresController do
+  it "return scores for terms" do
+    ScoreCache.stub(:for_term).with("microsoft") { 8.5 }
+    get :show, :term => "microsoft"
+    response.body.should == { :term => "microsoft", :score => 8.5 }.to_json
+  end
+end
