c6e972b4ef5c1e73f13182795e98bb8ff4b08424
Chris Neitzer <chrisn@soholaunch.com>
2012-07-05 11:05:58 -0400

    Removed test output from RockScore

--- app/services/rock_score.rb
+++ app/services/rock_score.rb
@@ -4,7 +4,6 @@ class RockScore
     positive = SearchEngine.count_results(%{"#{term} rocks"}).to_f
     negative = SearchEngine.count_results(%{"#{term} sucks"}).to_f
     score = 10 * positive / (positive + negative)
-    p [term, positive, negative, score]
     score.nan? ? NoScore : score
   end
 end
